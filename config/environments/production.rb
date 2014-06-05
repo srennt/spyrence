82aad953594f9872cccc1361ba4deb93ae4dbf74
Spyros Rennt <spyrosfotiou@gmail.com>
2014-06-05 19:33:21 +0200

    Add config/environments/production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,4 +80,17 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+config.action_mailer.raise_delivery_errors = true
+config.action_mailer.perform_deliveries = true
+config.action_mailer.delivery_method = :smtp
+config.action_mailer.smtp_settings = {
+    address:              'smtp.mail.yahoo.com',
+    port:                 587,
+    domain:               'yahoo.com',
+    user_name:            'rogerrabbit_careerfoundry',
+    password:             'Password1',
+    authentication:       'plain',
+    enable_starttls_auto: true
+  }
 end
