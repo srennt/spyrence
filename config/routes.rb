d0372cac6c6c2e3f32ee69ef23d9295cc69d3caa
Spyros Rennt <spyrosfotiou@gmail.com>
2014-06-05 19:34:27 +0200

    Add config/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,8 @@ Rails.application.routes.draw do
   
   get 'contact_out',  to: 'contact#contact_out'
 
+  get 'articles',   to: 'articles#new'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
