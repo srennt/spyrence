71c5854b2f9dfab5ac847e7eebb7aae34d650a26
Spyros Rennt <spyrosfotiou@gmail.com>
2014-06-07 21:42:30 +0200

    Add db.schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140516005703) do
+ActiveRecord::Schema.define(version: 20140607124132) do
+
+  create_table "articles", force: true do |t|
+    t.string   "title"
+    t.text     "text"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "products", force: true do |t|
     t.string   "name"
