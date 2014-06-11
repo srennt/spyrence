62054c2ca1979e71307b0fb3bb229ee4f5abf307
Spyros Rennt <spyrosfotiou@gmail.com>
2014-06-12 01:28:04 +0200

    Add app/controllers/reviews_controller.rb

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,29 @@
+class ReviewsController < ApplicationController
+  def new
+    @review = Review.new
+  end 
+
+  def create
+    @review = Review.new(review_params)
+ 
+  if @review.save
+    redirect_to @review
+  else
+    render 'new'
+  end
+  end
+
+   def show
+    @review = Review.find(params[:id])
+  end
+
+  def index
+    @reviews = Review.all
+  end
+
+  private
+    def review_params
+      params.require(:review).permit(:name, :email, :text)
+    end
+
+end
