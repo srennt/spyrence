e6b54ae75d0543e11c9b7fd539e214e5e1ca1270
Spyros Rennt <spyrosfotiou@gmail.com>
2014-06-28 17:40:51 +0200

    Add spec/factories.rb

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :product do
+    name "Mens 1"
+    description "black alpha flight jacket"
+    image_url "http://localhost:3000/images/mens1.jpg"
+    price "26"
+  end
+end
