fc8d1dcea8f9a068eca4ad5608e2027d4979f0f9
Spyros Rennt <spyrosfotiou@gmail.com>
2014-07-04 14:44:06 +0200

    Add app/controllers/users_controller.rb

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,22 @@
+class UsersControler < ApplicationController
+
+  authenticate_user!
+
+  load_and_authorize_resource
+
+  def show
+ end
+
+  def edit
+  end
+
+  def update
+    @user.update_attributes(user_params)
+  end
+
+  private
+
+  def user_params
+    require(:user)
+  end
+end
