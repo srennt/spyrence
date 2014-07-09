0c965d9da53af517b13c1cda7c17988c87f17c26
Spyros Rennt <spyrosfotiou@gmail.com>
2014-07-09 16:43:05 +0200

    Add spec/factories/users.rb

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,9 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
 FactoryGirl.define do
-  factory :user do
+  factory :user do |f|
+  	f.email "bla@blabla.com"
+    f.password "blax123bla"
+    f.password_confirmation "blax123bla"
   end
 end
