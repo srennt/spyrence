0654a58a33fef346d1d0a18c1b749af0a4f6a301
Spyros Rennt <spyrosfotiou@gmail.com>
2014-07-10 04:22:18 +0200

    Add spec/factories/users.rb

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,7 +3,7 @@
 FactoryGirl.define do
   factory :user do |f|
   	f.email "bla@blabla.com"
-    f.password "blax123bla"
-    f.password_confirmation "blax123bla"
+    f.username "blabla"
+    f.age "23"
   end
 end
