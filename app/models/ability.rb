9dfdc2db12d853a6fb98cb3319e3ead6bc894367
Spyros Rennt <spyrosfotiou@gmail.com>
2014-07-17 13:04:37 +0200

    Add app/models/ability.rb

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,6 +4,11 @@ class Ability
   def initialize(user)
     user ||= User.new # guest user (not logged in)
     can :manage, User, id: user.id
+    if user.is_admin
+      can :access, :rails_admin
+      can :dashboard
+    end
+    
     # Define abilities for the passed in user here. For example:
     #
     #   user ||= User.new # guest user (not logged in)
