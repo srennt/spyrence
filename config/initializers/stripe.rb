1e8ee8500eb5f0ff092a7d3bcfdd1ce688e9407e
Spyros Rennt <spyrosfotiou@gmail.com>
2014-07-25 14:52:43 +0200

    Add config/initializers/stripe.rb

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,6 +1,6 @@
 Rails.configuration.stripe = {
-  :publishable_key => ENV['pk_test_4Ry4kTY8cu0k3koeQpL0nlJS'],
-  :secret_key      => ENV['sk_test_4Ry4nAXW5unovr7qrhRO5lFX']
+  :publishable_key => ENV['STRIPE_PUBLISHABLE_KEY'],
+  :secret_key      => ENV['STRIPE_SECRET_KEY']
 }
 
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
